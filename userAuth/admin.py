738c25b793ba8ea4912597641710d514c9cbd321
madhu915 <mmadhumithaa09@gmail.com>
2023-03-21 12:25:36 +0530

    admin sites

--- userAuth/admin.py
+++ userAuth/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Intern, User, Mentor
+from .models import Comment, Intern, Task, User, Mentor
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Mentor)
 admin.site.register(Intern)
+admin.site.register(Task)
+admin.site.register(Comment)
