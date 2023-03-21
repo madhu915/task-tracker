dea2ee4be72e4bdb48f4a7faace87aaa78a00107
madhu915 <mmadhumithaa09@gmail.com>
2023-03-21 09:53:44 +0530

    update models -task and comments -del some files

--- userAuth/models.py
+++ userAuth/models.py
@@ -47,3 +47,36 @@ class Intern(models.Model):
 
     class Meta:
         db_table = 'interns'
+
+# tasks model
+
+class Task(models.Model):
+    internid = models.ForeignKey(Intern, on_delete=models.DO_NOTHING)
+    description = models.CharField(max_length=255, default='NA')
+    assigned_date = models.DateField(auto_now_add=True)
+    started_date = models.DateField(blank=True, null=True)
+    completed_date = models.DateField(blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)
+    completion_status = models.BooleanField(default=False)
+    progress_status = models.CharField(max_length=50)
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+    last_updated_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'tasks'
+
+# comments model
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    commenter = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    comment = models.TextField(null=False, blank=False)
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+
+    class Meta:
+        db_table = 'comments'
+
+    def __str__(self):
+        return f"Comment by {self.commenter.id} on {self.task.taskid}"
