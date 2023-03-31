8536f56c3a5c499b178ad6d8c80cbbd001721efb
madhu915 <mmadhumithaa09@gmail.com>
2023-04-01 01:09:15 +0530

    change password ui done

--- userAuth/static/js/userAuth.js
+++ userAuth/static/js/userAuth.js
@@ -23,12 +23,14 @@ var check = function () {
       document.getElementById("ico").style.display = "none";
       document.getElementById("cross").style.display = "none";
       document.getElementById("message").style.display = "none";
+      document.getElementById("reset").disabled=true;
     } else {
       document.getElementById("ico").style.display = "block";
       document.getElementById("cross").style.display = "none";
       document.getElementById("message").style.display = "block";
       document.getElementById("message").innerHTML = "Passwords Match!";
       document.getElementById("message").style.color = "rgb(21, 226, 21)";
+      document.getElementById("reset").disabled=false;
     }
   } else {
     document.getElementById("ico").style.display = "none";
@@ -36,5 +38,6 @@ var check = function () {
     document.getElementById("message").style.display = "block";
     document.getElementById("message").innerHTML = "Passwords do not match!";
     document.getElementById("message").style.color = "#ff2c39";
+    document.getElementById("reset").disabled=true;
   }
 };
