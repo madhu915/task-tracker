c2a64e8974d80c10bcae7f034e9c9159de501299
madhu915 <mmadhumithaa09@gmail.com>
2023-04-03 13:48:12 +0530

    redesign profile & reset password

--- userAuth/static/js/userAuth.js
+++ userAuth/static/js/userAuth.js
@@ -26,21 +26,21 @@ var check = function () {
     document.getElementById("password-verify").value
   ) {
     if (document.getElementById("password").value == "") {
-      document.getElementById("ico").style.display = "none";
-      document.getElementById("cross").style.display = "none";
+      document.getElementById("match").style.display = "none";
+      document.getElementById("mismatch").style.display = "none";
       document.getElementById("message").style.display = "none";
       document.getElementById("reset").disabled=true;
     } else {
-      document.getElementById("ico").style.display = "block";
-      document.getElementById("cross").style.display = "none";
+      document.getElementById("match").style.display = "block";
+      document.getElementById("mismatch").style.display = "none";
       document.getElementById("message").style.display = "block";
       document.getElementById("message").innerHTML = "Passwords Match!";
       document.getElementById("message").style.color = "rgb(21, 226, 21)";
       document.getElementById("reset").disabled=false;
     }
   } else {
-    document.getElementById("ico").style.display = "none";
-    document.getElementById("cross").style.display = "block";
+    document.getElementById("match").style.display = "none";
+    document.getElementById("mismatch").style.display = "block";
     document.getElementById("message").style.display = "block";
     document.getElementById("message").innerHTML = "Passwords do not match!";
     document.getElementById("message").style.color = "#ff2c39";
